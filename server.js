b16a739aa1f03d8b0ac5136ac2bd5b41d2dca75d
kennedymakokha <katchibo2@gmail.com>
2022-08-24 18:26:51 +0300

    fixing delivery prices per zone

--- server.js
+++ server.js
@@ -78,6 +78,9 @@ app.get('*', function (req, res) {
 });
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 4000) : 4000;
+// const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 4000) : 4000;
+// http.listen(port, () => console.log('Server listening on port ' + port));
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80):80;
 http.listen(port, () => console.log('Server listening on port ' + port));
+
 Riders(http)
