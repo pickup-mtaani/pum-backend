6640a580944eec3108e70f78fa15f25b1046c6a5
kennedymakokha <katchibo2@gmail.com>
2022-10-12 10:59:38 +0300

    doorstep

--- routes/door_step_package.routes.js
+++ routes/door_step_package.routes.js
@@ -54,10 +54,11 @@ router.put("/door-step/package/:id/:state", [authMiddleware, authorized], async 
 });
 router.get("/door-step-packages", [authMiddleware, authorized], async (req, res) => {
   try {
-    let
-    var searchKey = new RegExp(`${req.query.searchKey}`, 'i')
+
+
     let agent_packages
     if (req.query.searchKey) {
+      var searchKey = new RegExp(`${req.query.searchKey}`, 'i')
       agent_packages = await Door_step_Sent_package.find({ assignedTo: req.user._id, $or: [{ packageName: searchKey }, { receipt_no: searchKey }] }).sort({ createdAt: -1 }).limit(100)
         .populate('createdBy', 'f_name l_name name phone_number,');
       return res
