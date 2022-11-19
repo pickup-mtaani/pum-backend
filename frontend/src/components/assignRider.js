b68134dd8051225746584580a825ec19882fe511
kennedymakokha <katchibo2@gmail.com>
2022-11-19 17:35:16 +0300

    ah

--- frontend/src/components/assignRider.js
+++ frontend/src/components/assignRider.js
@@ -1,128 +1,172 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useCallback, forwardRef, useEffect, useState, useRef } from 'react';
+import { GoogleMapsProvider, useGoogleMap } from '@ubilabs/google-maps-react-hooks';
 
-import DataTable from 'react-data-table-component'
+
 import { connect } from 'react-redux'
 import { io } from 'socket.io-client'
 import { get_riders, fetchpackages } from '../redux/actions/riders.actions'
-import Search_filter_component from './common/Search_filter_component'
 import Layout from '../views/Layouts'
-import ReactMapGl, { Marker, Popup } from 'react-map-gl';
+import DirectionsService from './googlemaps/directionService'
+import MapMarkers from './googlemaps/mapMarkers';
 import PIN from './Riders/pin.png'
-import { useLocation } from 'react-router-dom'
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import AssignedPackModal from './Riders/AssignedPackModal'
-import { DownloadFile } from './common/helperFunctions'
+// import GoogleMapReact from 'google-map-react';
+import { useLocation } from 'react-router-dom'
+// import { GoogleMap } from 'react-google-maps/api';
 const socket = io("https://stagingapi.pickupmtaani.com/");
 
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2VuYXRlIiwiYSI6ImNqdWR0MjVsNzAxeTYzem1sb3FxaHhid28ifQ.ntUj7ZMNwUtKWaBUoUVuhw';
-function Users(props) {
 
 
-    const [viewPoints, setViewPoints] = useState({
-        width: "100vh", height: "800px", latitude: -1.286389, longitude: 36.817223, zoom: 16
-    })
-    const [filterText, setFilterText] = React.useState('');
-    const [searchValue, setSearchValue] = useState("")
-    const [date, setDate] = useState("")
-    const [popupMark, setlocapopup] = useState({
 
-    })
-    const [showModal, setShowModal] = useState(false);
-    const [resetPaginationToggle, setResetPaginationToggle] = React.useState(false)
-    const [RowsPerPage, setRowsPerPage] = useState(10)
-    const mapContainer = useRef(null);
-    const map = useRef(null);
+let start = {
+    lat: "-1.33124", long: "36.88484 "
+}
+let stop = {
+    lat: -1.145703, long: 36.964853
+}
+
+function Map(props) {
     const [lng, setLng] = useState(36.817223);
     const [lat, setLat] = useState(-1.286389);
-    const [zoom, setZoom] = useState(9);
-    const [totalRows, setTotalRows] = useState(0);
-    const [data, setFilterData] = React.useState([]);
-    const [show, togglePopup] = useState();
-    const [item, setItem] = useState([]);
+    const [origin, setOrigin] = useState({ lat: -1.2878412, lng: 36.8278173 })
+    const [destination, setDestination] = useState({ lat: -1.2850204, lng: 36.8259191 })
+    const [rider, setrider] = useState("");
     const location = useLocation()
-    const onChangeFilter = (e) => {
-        setFilterText(e)
+    const map = useGoogleMap()
+    const markerRef = useRef()
 
-    }
+    const defaultProps = {
+        center: {
+            lat: -1.286389,
+            lng: 36.817223,
+        },
+        zoom: 16
+    };
 
-    const fetchUserPackages = async (row) => {
-        setShowModal(true);
-        setItem(row)
-        let result = await props.fetchpackages(row._id)
-        setFilterData(result)
-    }
+    // useEffect(() => {
+    //     if (!map || markerRef.current) return;
+    //     const options = {
+    //         map
+    //     };
+    //     markerRef.current = new window.google.maps.Marker(options)
+    // }, [map]);
+
+    // useEffect(() => {
+    //     if (!markerRef.current) return;
+    //     markerRef.current.setPosition({
+    //         lat: -1.286389,
+    //         lng: 36.817223,
+
+    //     })
+    //     map.panTo({
+    //         lat: -1.286389,
+    //         lng: 36.817223,
+
+    //     })
+    // }, [map]);
 
 
     useEffect(() => {
-
         socket.on('connection');
-
-        // socket.emit('track_rider', { rider_id: '', user_id: "1322" });
+        // console.log(location.state.id)
         socket.emit('track_rider', { rider_id: location.state.id, user_id: "1322" });
-        socket.on('user-joined', data => {
-            console.log('user joined!', data);
+        // socket.on('position-change', data => {
+        //     console.log('Position changed!!: ', data);
+        // });
+        socket.on('position-changed', coordinates => {
+            console.log("E", coordinates.coordinates)
+            setLng(coordinates.coordinates.longitude)
+            setLat(coordinates.coordinates.latitude)
+            let obj = { lat: coordinates.coordinates.latitude, lng: coordinates.coordinates.longitude }
+            console.log(obj)
+            setDestination(obj)
         });
 
-        socket.on('get-users', data => {
-            console.log('Get users:', data);
-        });
+        if (!markerRef.current) return;
+        markerRef.current.setPosition({
+            lat: -1.286389,
+            lng: 36.817223,
 
-        // getCurrentLocation();
-        // socket.emit('position-change', {coordinates: crd});
+        })
+        map.panTo({
+            lat: -1.286389,
+            lng: 36.817223,
 
-        socket.on('position-changed', async d => {
-            console.log('coordinates', d.coordinates);
-            setLng(d.coordinates.longitude)
-            setLat(d.coordinates.latitude)
-        });
+        })
+    }, [socket])
 
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: zoom
-        });
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
-        });
-    }, [lat, lng])
+    const [mapContainer, setMapContainer] = useState(null);
+    const mapRef = useCallback(node => {
+        node && setMapContainer(node);
+    }, []);
+
+    const mapOptions = {
+        center: { lat: -1.286389, lng: 36.817223 },
+        zoom: 13,
+        disableDefaultUI: true,
+        zoomControl: true,
+        zoomControlOptions: {
+            position: 3 // Right top
+        }
+    };
+
     return (
         <Layout>
-            <div className=" mx-2 ">
-                Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-                <div ref={mapContainer} className="map-container my-2" style={{ height: '400px' }} />
-                {/* <div ref={mapContainer} className="map-container" /> */}
-                <ReactMapGl
-                    {...viewPoints}
-                    mapStyle="mapbox://styles/mapbox/streets-v11"
-                    onViewPortsChange={(viewPoints) => setViewPoints(viewPoints)}
-                    mapboxAccessToken="pk.eyJ1Ijoia2VuYXRlIiwiYSI6ImNqdWR0MjVsNzAxeTYzem1sb3FxaHhid28ifQ.ntUj7ZMNwUtKWaBUoUVuhw"
-                >
-                    <Marker
-                        latitude={lat}
-                        longitude={lng}
-                    // offsetLeft={-20}
-                    // offsetTop={-20}
-                    >
-                        <img
-                            style={{ cursor: 'pointer', height: 20, width: 40, objectFit: 'cover' }}
 
-                            src={PIN}
-                        />
-                    </Marker>
+            <GoogleMapsProvider
+                googleMapsAPIKey="AIzaSyBBYlYdpbci4zBhCSyLAJngOBLR3cRCGJA"
+                mapContainer={mapContainer}
+                mapOptions={mapOptions}>
+                <React.StrictMode>
+                    <div ref={mapRef} style={{ height: '100%' }} />
+                    <DirectionsService origin={origin} destination={destination} />
+                    <MapMarkers origin={origin} destination={destination} />
+                </React.StrictMode>
+            </GoogleMapsProvider>
+            {/* <div style={{ height: '100vh', width: '100%' }}> */}
+            {/* <GoogleMap
+                    id="circle-example"
+                    mapContainerStyle={{
+                        height: "400px",
+                        width: "800px"
+                    }}
+                    zoom={7}
+                    center={{
+                        lat: -3.745,
+                        lng: -38.523
+                    }}
+                /> */}
 
-                </ReactMapGl>
-            </div>
+
+            {/* <GoogleMapReact
+                    bootstrapURLKeys={{ key: "" }}
+                    // bootstrapURLKeys={{ key: "AIzaSyBBYlYdpbci4zBhCSyLAJngOBLR3cRCGJA" }}
+                    defaultCenter={{ lat, lng }}
+                    defaultZoom={defaultProps.zoom}
+                > */}
+            {/* <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyBBYlYdpbci4zBhCSyLAJngOBLR3cRCGJA' }}
+                    defaultCenter={defaultProps.center}
+                    defaultZoom={defaultProps.zoom}
+                // yesIWantToUseGoogleMapApiInternals
+                // onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+                > */}
+            {/* <AnyReactComponent
+                        lat={lat}
+                        lng={lng}
+                        text="My Marker"
+                    />
+
+                </GoogleMapReact> */}
+            {/* </div> */}
+
         </Layout>
     )
 }
 
-Users.propTypes = {}
+Map.propTypes = {}
 
 
 const mapStateToProps = (state) => {
@@ -134,5 +178,5 @@ const mapStateToProps = (state) => {
     };
 };
 
-export default connect(mapStateToProps, { get_riders, fetchpackages })(Users)
+export default connect(mapStateToProps, { get_riders, fetchpackages })(Map)
 
