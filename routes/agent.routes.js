b9f51717e92a67f8c257be5e52b60f37ca7ae5a0
kennedymakokha <katchibo2@gmail.com>
2022-11-23 07:35:26 +0300

    edit agent

--- routes/agent.routes.js
+++ routes/agent.routes.js
@@ -302,7 +302,7 @@ router.get('/agents-grouped', async (req, res) => {
             // }
         ])
         return res.status(200).json(t);
-        console.log(t)
+
     } catch (error) {
         console.log(error)
         return res.status(400).json({ success: false, message: 'operation failed ', error });
