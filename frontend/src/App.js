adb18800c309037777fe77526ed9b30e8cc19898
Pekstaar <pekstaar@gmail.com>
2023-02-03 13:59:32 +0300

    [UPDATE]: withdrawals populate doorstep,shelf

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import Login from "./login";
-import {
-  BrowserRouter as Router,
-  useRoutes,
-} from "react-router-dom";
+import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import Dashboard from "./components/dashboard";
 import Sellers from "./components/Seller";
 import UserDetails from "./components/Seller/UserDetails";
 import Products from "./components/Seller/products";
-import Packages from "./components/packages"
-import AgentPackages from "./components/packages/aent"
+import Packages from "./components/packages";
+import AgentPackages from "./components/packages/aent";
 import Riders from "./components/Riders";
 import Payments from "./components/payment";
-import Tracks from "./components/tracks ";
+// import Tracks from "./components/tracks ";
 import WareHouse from "./components/warehouse";
 import Assign_Agent_package_riderWareHouse from "./components/warehouse/agent/assignRider";
-import Agents from "./components/agents ";
+// import Agents from "./components/agents ";
 import WHdoorstep from "./components/warehouse/doorstep";
 import PickFromRider from "./components/warehouse/doorstep/pick";
 import PickFromRiderPage from "./components/warehouse/doorstep/riderpage";
@@ -30,13 +27,12 @@ import PickErrandFromRiderPage from "./components/warehouse/errand/riderpage";
 import GiveErrandtoRiderPage from "./components/warehouse/errand/give";
 import AssignErrandRiderPage from "./components/warehouse/errand/give";
 
-
 import WHAgent from "./components/warehouse/agent";
 import Collections from "./components/routes";
 import WareHouseActions from "./components/warehouse/actionPage";
 
 import Riderpage from "./components/warehouse/riderpage";
-import AgentsDetails from "./components/agents /agentsDetails";
+// import AgentsDetails from "./components/agents/agentsDetails";
 import SwitchBoard from "./components/packages/switchboard";
 import AssignRider from "./components/TrackRider";
 
@@ -46,8 +42,7 @@ import RentShelf from "./components/rentshelf";
 import RentShelfAgents from "./components/rentshelf/business";
 import RentShelfAgent from "./components/rentshelf/details";
 
-
-import Setup from './components/Setup'
+import Setup from "./components/Setup";
 const App = () => {
   let routes = useRoutes([
     { path: "/", element: <Login /> },
@@ -63,40 +58,66 @@ const App = () => {
 
     { path: "/rent-a-shelf", element: <RentShelf /> },
     { path: "/rent-a-shelf/:shop/businesses", element: <RentShelfAgents /> },
-    { path: "/rent-a-shelf/:shop/business/:business", element: <RentShelfAgent /> },
+    {
+      path: "/rent-a-shelf/:shop/business/:business",
+      element: <RentShelfAgent />,
+    },
 
-    { path: "/tracks", element: <Tracks /> },
+    // { path: "/tracks", element: <Tracks /> },
     { path: "/wahehouse", element: <WareHouse /> },
     { path: "/wahehouse/:slug", element: <WareHouseActions /> },
     { path: "/wahehouse/assign/:rider", element: <Riderpage /> },
 
-    { path: "/wahehouse/agent-agent/pick-package-from-rider/:rider", element: <PickFromAgentRider /> },
+    {
+      path: "/wahehouse/agent-agent/pick-package-from-rider/:rider",
+      element: <PickFromAgentRider />,
+    },
     { path: "/wahehouse/agent-agent/packages", element: <WHAgent /> },
-    { path: "/wahehouse/agent-agent/assign-package-to/:rider", element: <Assign_Agent_package_riderWareHouse /> },
+    {
+      path: "/wahehouse/agent-agent/assign-package-to/:rider",
+      element: <Assign_Agent_package_riderWareHouse />,
+    },
     { path: "/wahehouse/agent-agent/:slug/agents", element: <RiderAgents /> },
 
     { path: "/wahehouse/doorstep/packages", element: <WHdoorstep /> },
     { path: "/wahehouse/doorstep/pick-packages", element: <PickFromRider /> },
-    { path: "/wahehouse/doorstep/pick-packages-from/:rider", element: <PickFromRiderPage /> },
-    { path: "/wahehouse/doorstep/give-package-to-rider", element: <GIvetoRiderPage /> },
+    {
+      path: "/wahehouse/doorstep/pick-packages-from/:rider",
+      element: <PickFromRiderPage />,
+    },
+    {
+      path: "/wahehouse/doorstep/give-package-to-rider",
+      element: <GIvetoRiderPage />,
+    },
     { path: "/wahehouse/doorstep/assign-rider", element: <AsRiderPage /> },
 
     { path: "/business", element: <Bussiness /> },
 
     { path: "/wahehouse/errand/packages", element: <WereHouseErrands /> },
-    { path: "/wahehouse/errand/pick-packages", element: <PickErrandFromRider /> },
-    { path: "/wahehouse/errand/pick-packages-from/:rider", element: <PickErrandFromRiderPage /> },
-    { path: "/wahehouse/errand/give-package-to-rider", element: <GiveErrandtoRiderPage /> },
-    { path: "/wahehouse/errand/assign-rider", element: <AssignErrandRiderPage /> },
+    {
+      path: "/wahehouse/errand/pick-packages",
+      element: <PickErrandFromRider />,
+    },
+    {
+      path: "/wahehouse/errand/pick-packages-from/:rider",
+      element: <PickErrandFromRiderPage />,
+    },
+    {
+      path: "/wahehouse/errand/give-package-to-rider",
+      element: <GiveErrandtoRiderPage />,
+    },
+    {
+      path: "/wahehouse/errand/assign-rider",
+      element: <AssignErrandRiderPage />,
+    },
 
     { path: "/setup", element: <Setup /> },
 
     { path: "/switch-board", element: <SwitchBoard /> },
     { path: "/track/:riders", element: <AssignRider /> },
 
-
-    { path: "/agents", element: <Agents /> },
-    { path: "/agent/:name", element: <AgentsDetails /> },
+    // { path: "/agents", element: <Agents /> },
+    // { path: "/agent/:name", element: <AgentsDetails /> },
 
     // ...
   ]);
