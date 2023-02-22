cc3344d3429f18e2922b28f2331e9ce39ab8f629
Pekstaar <pekstaar@gmail.com>
2023-02-22 07:10:02 +0300

    [UPDATE]: mysql config

--- helpers/TransactionQuery.js
+++ helpers/TransactionQuery.js
@@ -4,13 +4,34 @@ const queryTransaction = require("../mysql/QueryTransaction");
 
 const handleTransactionQuery = async ({ transactionId }) => {
   try {
-    const res = queryTransaction(transactionId);
-    console.log("RESPONSE: ", res);
-    if (res?.id && res?.TransID === transactionId) {
-      return { successful: true, message: "Transaction successful" };
-    } else {
-      return { successful: false, message: "Transaction not successful" };
-    }
+    const connection = createConnection();
+    connection.connect((err) => {
+      if (err) {
+        console.error("Error connecting to database: ", err);
+      } else {
+        console.log("Connected to MYSQL!");
+      }
+    });
+
+    connection.query(
+      `SELECT * FROM mpesa_data where TransID='RBL15O0FCP'`,
+      (err, results) => {
+        if (err) {
+          console.error("Error selecting from database: ", err);
+          return { error: err?.message, data: null, success: false };
+        } else {
+          console.log("RESULTS:", results);
+
+          if (results[0]?.id && results[0]?.TransID === transactionId) {
+            return { successful: true, message: "Transaction successful" };
+          } else {
+            return { successful: false, message: "Transaction not successful" };
+          }
+        }
+      }
+    );
+
+    connection.end();
   } catch (error) {
     console.log(
       "TRANSACTION QUERY ERROR ERROR:",
